40172ec3d8f0b1a082c8b2734776df7e239021d0
BringFeel <69062690+BringFeel@users.noreply.github.com>
2025-02-24 17:49:33 -0300

    Add files via upload

--- assets/js/bots.js
+++ assets/js/bots.js
@@ -1,32 +1,19 @@
-				const urlCH = 'https://cors.unmutedte.ch/https://disstat-api.tomatenkuchen.com/api/bots/chitopanbot?timezone=America/Buenos_Aires&locale=es-419&start=2023-07-17T0000&dataPoints=90';
-				const urlKF = 'https://cors.unmutedte.ch/https://disstat-api.tomatenkuchen.com/api/bots/koofa?timezone=America/Buenos_Aires&locale=es-419&start=2023-07-17T0000&dataPoints=90';
+const apiURL = 'https://cors-anywhere.bringfeel.com.ar/https://api.bringfeel.com.ar/chitopanbot/getStatus?startDate=2024-01-16%2013:01:01&dataPoints=2';
 
-				const requestOptions = {
-					method: 'GET',
-					headers: {
-						'Content-Type': 'application/json',
-						'Origin': 'https://bringfeel.com.ar/',
-					},
-				};
+	const requestOptions = {
+		method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				'Origin': 'https://bringfeel.com.ar/',
+		},
+	};
 
-				function ChitoPanBOT() {
-  				  fetch(urlCH, requestOptions).then(response => {
-					return response.json();
-				}).then(data => {
-					var servers_chitopanbot = document.getElementById("servers-chitopanbot");
-	                                servers_chitopanbot.innerHTML = data.charts[0].data[data.charts[0].data.length - 1];
-					
-				})
-			    }
-                ChitoPanBOT()
+	function updateBot(url, options) {
+		fetch(url, options).then(response => {return response.json();
+		}).then(data => {
+			document.getElementById("servers-chitopanbot").innerHTML = data.charts.serverCount.data[1];;
+			document.getElementById("servers-koofa").innerHTML = '???';
+		})
+	}
 
-				function Koofa() {
-  				  fetch(urlKF, requestOptions).then(response => {
-					return response.json();
-				}).then(data => {
-					var servers_koofa = document.getElementById("servers-koofa");
-	                                servers_koofa.innerHTML = data.charts[0].data[data.charts[0].data.length - 1];
-					
-				})
-			    }
-                Koofa()
+    updateBot(apiURL, requestOptions)
